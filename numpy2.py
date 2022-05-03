ffa5fe217e03e14fa947ad60d1d8aa69f06ce889
taoyngwie <taoguitardz@gmil.com>
2022-05-04 01:19:48 +0700

    first commit

---
+++ numpy2.py
@@ -0,0 +1,18 @@
+#ตอนที่ 4 - การสร้าง Array 2 มิติ
+
+import numpy as np
+
+#การสร้างเเบบ 1 มิติ
+arr1 = np.array([1,2,3,4])
+#แสดงอาเรย์
+print(arr1)
+#แสดงมิติ
+print(arr1.ndim)
+
+#การสร้างเเบบ 2 มิติ
+arr2 = np.array([[1,2,3],[4,5,6]])
+#แสดงอาเรย์
+print(arr2)
+#แสดงมิติ
+print(arr2.ndim)
+
